11cb56199811661f7e729a4bd104583c34812eed
Dmitry Gerasimchuk <arhdigger@hotmail.com>
2023-02-13 00:13:15 +0300

    Сохранение папок с решениями задач из домашней задания

---
+++ Task101/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1:
+// Напишите программу,
+// которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+// Проверка:
+// a = 5; b = 7 ->  max = 7, min = 5
+// a = -9 b = -3 -> max = -3, min = -9
+
+System.Console.WriteLine("Программа для нахождения минимального и максимального числа:");
+
+System.Console.WriteLine("Введите первое число > ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число > ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    System.Console.WriteLine($"Введенное число {number1} больше введенного числа {number2}.");
+    System.Console.WriteLine($"Или другими словами: {number1} является максимумом, а {number2} - минимумом.");
+}
+else
+{
+    System.Console.WriteLine($"Введенное число {number2} больше введенного числа {number1}.");
+    System.Console.WriteLine($"Или другими словами: {number1} является минимумом, а {number2} - максимумом.");
+}
---
+++ Task102/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 2:
+// Напишите программу,
+// которая принимает на вход три числа и
+// выдаёт максимальное из этих чисел.
+
+// Проверка:
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+System.Console.WriteLine("Программа для нахождения максимума для трех введенных чисел:");
+
+System.Console.WriteLine("Введите первое число > ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число > ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите третье число > ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number2 > max)
+{
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+
+System.Console.WriteLine($"В введенной тройке чисел максимальное число: {max}");
